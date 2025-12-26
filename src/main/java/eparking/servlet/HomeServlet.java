7016829e9ed7badd565413f09784b8fa0d16059a
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-12-26 17:52:46 -0500

    feat: Protecting the /home route

--- src/main/java/eparking/servlet/HomeServlet.java
+++ src/main/java/eparking/servlet/HomeServlet.java
@@ -5,6 +5,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
 /**
@@ -14,20 +16,23 @@ import java.io.IOException;
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
     public HomeServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession(false);
+		
+		if (session == null || session.getAttribute("userlogged") == null) {
+            response.sendRedirect(request.getContextPath() + "/login");
+            return; 
+        }
+		
+		request.getRequestDispatcher("home.jsp").forward(request, response);
 	}
 
 }
